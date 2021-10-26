bbb406e63799854b497e619de58fb0a2c49a4710
Aryamaan Kunwar <aryamaan3@gmail.com>
2021-10-26 12:15:57 +0200

    td1

---
+++ progAvancé/src/miage/TDClassesInternes/Traitement_1.java
@@ -0,0 +1,45 @@
+package miage.TDClassesInternes;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class Traitement_1 implements Consumer<UE> {
+
+    @Override
+    public void accept(UE ue) {
+        ue.annee++;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<UE> listOfUE = new ArrayList<>();
+        UE UE_1 = new UE("Prog Avancé", "MIAGE", "Lahire", "QCM", 1980, false);
+        UE UE_2 = new UE("Tech Web", "MIAGE", "Buffa", "Projets", 2000, true);
+        UE UE_3 = new UE("Algo", "MIAGE", "Kounalis", "Synthése", 1999, false);
+        UE UE_4 = new UE("Maths", "MIAGE", "Formenti", "QCM", 1800, true);
+        UE UE_5 = new UE("IA", "MIAGE", "Cabrio", "Projets", 2013, true);
+        listOfUE.add(UE_1);
+        listOfUE.add(UE_2);
+        listOfUE.add(UE_3);
+        listOfUE.add(UE_4);
+        listOfUE.add(UE_5);
+
+        Traitement_1 t = new Traitement_1();
+
+        //listOfUE.forEach(t); non statique
+        //System.out.println(listOfUE);
+
+        class Traitement_1_bis implements Consumer<UE>{
+
+            @Override
+            public void accept(UE ue) {
+                ue.annee++;
+            }
+        }
+
+        Traitement_1_bis t1bis = new Traitement_1_bis();
+        //listOfUE.forEach(t1bis); //locale
+        //System.out.println(listOfUE);
+
+
+    }
+}
---
+++ progAvancé/src/miage/TDClassesInternes/UE.java
@@ -0,0 +1,65 @@
+package miage.TDClassesInternes;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class UE {
+    String nom, nomDiplome, nomEnseignant, typeControle;
+    public Integer annee;
+    Boolean rattrapage;
+    addAnnee add;
+    UE(String nom, String nomDiplome, String nomEnseignant, String typeControle, Integer annee, Boolean rattrapage){
+        this.nom = nom;
+        this.nomDiplome = nomDiplome;
+        this.nomEnseignant = nomEnseignant;
+        this.typeControle = typeControle;
+        this.annee = annee;
+        this.rattrapage = rattrapage;
+        add = new addAnnee();
+    }
+
+
+    class addAnnee implements Consumer<UE> {
+
+        @Override
+        public void accept(UE ue) {
+            ue.annee ++;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "UE{" +
+                "nom='" + nom + '\'' +
+                ", nomDiplome='" + nomDiplome + '\'' +
+                ", nomEnseignant='" + nomEnseignant + '\'' +
+                ", typeControle='" + typeControle + '\'' +
+                ", annee=" + annee +
+                ", rattrapage=" + rattrapage +
+                '}' + '\n';
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList <UE> listOfUE = new ArrayList<>();
+        UE UE_1 = new UE("Prog Avancé", "MIAGE", "Lahire", "QCM", 1980, false);
+        UE UE_2 = new UE("Tech Web", "MIAGE", "Buffa", "Projets", 2000, true);
+        UE UE_3 = new UE("Algo", "MIAGE", "Kounalis", "Synthése", 1999, false);
+        UE UE_4 = new UE("Maths", "MIAGE", "Formenti", "QCM", 1800, true);
+        UE UE_5 = new UE("IA", "MIAGE", "Cabrio", "Projets", 2013, true);
+        listOfUE.add(UE_1);
+        listOfUE.add(UE_2);
+        listOfUE.add(UE_3);
+        listOfUE.add(UE_4);
+        listOfUE.add(UE_5);
+
+        //listOfUE.forEach((n) -> n.annee++); //lambda
+
+        //STATIC
+        //add static to addAnnee
+        //addAnnee t = new addAnnee();
+        //listofUE.forEach(t)
+
+    }
+}
